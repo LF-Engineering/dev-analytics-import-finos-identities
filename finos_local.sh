70ea3f65e080110f4805074481313cd420b940be
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-02-05 08:32:09 +0000

    Prod deployment
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- finos_local.sh
+++ finos_local.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+if [ -z "${IMPORT_DIR}" ]
+then
+  export IMPORT_DIR="/root/go/src/github.com/LF-Engineering/dev-analytics-import-finos-identities"
+fi
+cd "${IMPORT_DIR}" || exit 1
 MISSING_ORGS_CSV=finos_missing_orgs MISSING_PROFILES_CSV=finos_missing_profiles ORGS_MAP_FILE=./map_org_names.yaml REPLACE=1 COMPARE=1 PROJECT_SLUG=finos-f SH_DSN="`cat ./DB_CONN.local.secret`" ./import-identities.sh local
--- import-identities.sh
+++ import-identities.sh
@@ -8,7 +8,7 @@ fi
 lock_file="/tmp/import-identities-${1}.lock"
 if [ -f "${lock_file}" ]
 then
-  echo "$0: another import-identities \"$1\" instance is still running, exiting"
+  echo "$0: another import-identities \"${1}\" instance is still running, exiting"
   exit 2
 fi
 if [ -z "${IMPORT_DIR}" ]
@@ -39,5 +39,6 @@ git clone https://github.com/LF-Engineering/dev-analytics-affiliation || exit 8
 cp dev-analytics-finos-metadata/ssf-bitergia-affiliations.yaml ./identities.yaml || exit 9
 cp dev-analytics-affiliation/map_org_names.yaml ./map_org_names.yaml || exit 10
 remove_clones
-echo 'would go!'
-#./import-identities ./identities.yaml
+date
+echo 'Running import-identities'
+./import-identities ./identities.yaml
